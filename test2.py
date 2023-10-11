11a8bfa0f6acc24b9ee0a7f57839050dc0a79c8d
Nayrouz Tarik <107729348+NayrouzTarik@users.noreply.github.com>
2023-10-11 20:17:28 +0100

    ready

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
+# a program that calculate the square of a number that the user must choose, if the user choose 0 you have to exit
 while True:
     nbr = int(input("Give the number you want : "))
     if nbr == 0:
